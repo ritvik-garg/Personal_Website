af5768fe9116a69e245d71ac1382201b557107e1
ritvik-garg <ritvik.garg@students.iiit.ac.in>
2020-10-11 14:27:52 +0530

    added README, updated all html files

--- JS/script.js
+++ JS/script.js
@@ -3,3 +3,14 @@ function myFunction() {
         'resources/SSD_Assignment_2.pdf'
     );
 }
+
+// function blink_text() {
+//     $('.blink').fadeOut(100);
+//     $('.blink').fadeIn(100);
+// }
+// setInterval(blink_text, 500);
+
+var blink = document.getElementById('blink');
+setInterval(function() {
+    blink.style.opacity = (blink.style.opacity == 0 ? 1 : 0);
+}, 100);
